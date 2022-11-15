33bbb78365c6ef56d51369b57cef904e46356fe1
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-11-15 15:42:58 +0530

    Add src

---
+++ rust/resistor-color/src/lib.rs
@@ -0,0 +1,28 @@
+#[derive(Debug, PartialEq, Eq)]
+pub enum ResistorColor {
+    Black,
+    Blue,
+    Brown,
+    Green,
+    Grey,
+    Orange,
+    Red,
+    Violet,
+    White,
+    Yellow,
+}
+
+pub fn color_to_value(_color: ResistorColor) -> u32 {
+    unimplemented!("convert a color into a numerical representation")
+}
+
+pub fn value_to_color_string(value: u32) -> String {
+    unimplemented!(
+        "convert the value {} into a string representation of color",
+        value
+    )
+}
+
+pub fn colors() -> Vec<ResistorColor> {
+    unimplemented!("return a list of all the colors ordered by resistance")
+}
