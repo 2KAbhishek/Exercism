8c96f7570a74f5f45865c7b4e9cd486833cfc74c
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-11-15 16:01:03 +0530

    color to value passes

--- rust/resistor-color/src/lib.rs
+++ rust/resistor-color/src/lib.rs
@@ -1,19 +1,19 @@
 #[derive(Debug, PartialEq, Eq)]
 pub enum ResistorColor {
-    Black,
-    Blue,
-    Brown,
-    Green,
-    Grey,
-    Orange,
-    Red,
-    Violet,
-    White,
-    Yellow,
+    Black = 0,
+    Brown = 1,
+    Red = 2,
+    Orange = 3,
+    Yellow = 4,
+    Green = 5,
+    Blue = 6,
+    Violet = 7,
+    Grey = 8,
+    White = 9,
 }
 
 pub fn color_to_value(_color: ResistorColor) -> u32 {
-    unimplemented!("convert a color into a numerical representation")
+    _color as u32
 }
 
 pub fn value_to_color_string(value: u32) -> String {
