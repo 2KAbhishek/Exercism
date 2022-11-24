7cda7766547f32e604e0bfea7b8128829f1de5c5
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-11-25 02:34:52 +0530

    All tests pass

--- rust/rpn-calculator/tests/rpn-calculator.rs
+++ rust/rpn-calculator/tests/rpn-calculator.rs
@@ -1,4 +1,4 @@
-use rpn_calculator::*;
+use rpn_calculator::{CalculatorInput, evaluate};
 
 fn calculator_input(s: &str) -> Vec<CalculatorInput> {
     s.split_whitespace()
@@ -19,70 +19,60 @@ fn test_empty_input_returns_none() {
 }
 
 #[test]
-#[ignore]
 fn test_simple_value() {
     let input = calculator_input("10");
     assert_eq!(evaluate(&input), Some(10));
 }
 
 #[test]
-#[ignore]
 fn test_simple_addition() {
     let input = calculator_input("2 2 +");
     assert_eq!(evaluate(&input), Some(4));
 }
 
 #[test]
-#[ignore]
 fn test_simple_subtraction() {
     let input = calculator_input("7 11 -");
     assert_eq!(evaluate(&input), Some(-4));
 }
 
 #[test]
-#[ignore]
 fn test_simple_multiplication() {
     let input = calculator_input("6 9 *");
     assert_eq!(evaluate(&input), Some(54));
 }
 
 #[test]
-#[ignore]
 fn test_simple_division() {
     let input = calculator_input("57 19 /");
     assert_eq!(evaluate(&input), Some(3));
 }
 
 #[test]
-#[ignore]
 fn test_complex_operation() {
     let input = calculator_input("4 8 + 7 5 - /");
     assert_eq!(evaluate(&input), Some(6));
 }
 
 #[test]
-#[ignore]
 fn test_too_few_operands_returns_none() {
     let input = calculator_input("2 +");
     assert_eq!(evaluate(&input), None);
 }
 
 #[test]
-#[ignore]
 fn test_too_many_operands_returns_none() {
     let input = calculator_input("2 2");
     assert_eq!(evaluate(&input), None);
 }
 
 #[test]
-#[ignore]
 fn test_zero_operands_returns_none() {
     let input = calculator_input("+");
     assert_eq!(evaluate(&input), None);
 }
 
 #[test]
-#[ignore]
 fn test_intermediate_error_returns_none() {
     let input = calculator_input("+ 2 2 *");
     assert_eq!(evaluate(&input), None);
