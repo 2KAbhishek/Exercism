db56b84b378b826bec80321354feca79d158070f
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-12-01 17:42:56 +0530

    Implement solution

--- ruby/lasagna/lasagna.rb
+++ ruby/lasagna/lasagna.rb
@@ -1,13 +1,14 @@
 class Lasagna
+  EXPECTED_MINUTES_IN_OVEN = 40
   def remaining_minutes_in_oven(actual_minutes_in_oven)
-    raise 'Please implement the Lasagna#remaining_minutes_in_oven method'
+    EXPECTED_MINUTES_IN_OVEN - actual_minutes_in_oven
   end
 
   def preparation_time_in_minutes(layers)
-    raise 'Please implement the Lasagna#preparation_time_in_minutes method'
+    layers * 2
   end
 
   def total_time_in_minutes(number_of_layers:, actual_minutes_in_oven:)
-    raise 'Please implement the Lasagna#total_time_in_minutes method'
+    actual_minutes_in_oven + preparation_time_in_minutes(number_of_layers)
   end
 end
