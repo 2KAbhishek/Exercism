5e87ef43471042b5205a85effcfba09f525d0fc7
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-05-14 01:58:10 +0530

    refactor: get unit

--- typescript/resistor-color-trio/resistor-color-trio.ts
+++ typescript/resistor-color-trio/resistor-color-trio.ts
@@ -1,14 +1,18 @@
-export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
-export const UNITS = ["", "kilo", "mega", "giga", "tera", "peta", "exa", "zetta", "yotta"];
+const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
+const UNITS = ["", "kilo", "mega", "giga", "tera", "peta", "exa", "zetta", "yotta"];
 
 export function decodedResistorValue(colors: string[]): string {
     let value = getValues(colors);
-    const unit = UNITS[Math.floor(Math.log10(value) / 3)];
+    const unit = getUnit(value);
     value /= Math.pow(10, Math.floor(Math.log10(value) / 3) * 3);
     return `${value || 0} ${unit || ""}ohms`;
 }
 
-export function getValues(colors: string[]): number {
+function getUnit(value: number): string {
+    return UNITS[Math.floor(Math.log10(value) / 3)];
+}
+
+function getValues(colors: string[]): number {
     const [first, second, third] = colors;
     return (COLORS.indexOf(first) * 10 + COLORS.indexOf(second)) * Math.pow(10, COLORS.indexOf(third));
 }
