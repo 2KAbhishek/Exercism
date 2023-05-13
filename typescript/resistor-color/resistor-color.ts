eb714544c755392ce2572e1d6e1231993b868874
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-05-14 00:56:45 +0530

    feat: all tests pass

--- typescript/resistor-color/resistor-color.ts
+++ typescript/resistor-color/resistor-color.ts
@@ -1,5 +1,6 @@
+export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
+
 export const colorCode = (color: string): number => {
-  return 0;
+    return COLORS.indexOf(color);
 }
 
-export const COLORS = undefined
