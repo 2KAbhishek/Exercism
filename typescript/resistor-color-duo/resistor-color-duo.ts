5394cb77ee341ac6844785e6bd8893b493ab394d
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-05-14 00:27:56 +0530

    feat: implement solution

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,3 +1,5 @@
-export function decodedValue() {
-  throw new Error('Remove this statement and implement this function')
+const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+
+export function decodedValue(colors: string[]): number {
+    return COLORS.indexOf(colors[0]) * 10 + COLORS.indexOf(colors[1])
 }
