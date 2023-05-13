be29c1488bad8f5af2e143775e195edf481afb92
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-05-14 01:48:19 +0530

    feat: all tests pass

--- typescript/resistor-color-trio/resistor-color-trio.ts
+++ typescript/resistor-color-trio/resistor-color-trio.ts
@@ -1,3 +1,10 @@
-export function decodedResistorValue() {
-  throw new Error('Remove this statement and implement this function')
+export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
+export const UNITS = ["", "kilo", "mega", "giga", "tera", "peta", "exa", "zetta", "yotta"];
+
+export function decodedResistorValue(colors: string[]): string {
+    const [first, second, third] = colors;
+    let value = (COLORS.indexOf(first) * 10 + COLORS.indexOf(second)) * Math.pow(10, COLORS.indexOf(third));
+    const unit = UNITS[Math.floor(Math.log10(value) / 3)];
+    value /= Math.pow(10, Math.floor(Math.log10(value) / 3) * 3);
+    return `${value || 0} ${unit || ""}ohms`;
 }
