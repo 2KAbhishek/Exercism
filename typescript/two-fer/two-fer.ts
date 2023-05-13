893b749f206a0620cbd916e5a685185f6e20d858
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-05-13 23:40:57 +0530

    docs: cleanup comments

--- typescript/two-fer/two-fer.ts
+++ typescript/two-fer/two-fer.ts
@@ -1,7 +1,3 @@
-/**
- * This stub is provided to make it straightforward to get started.
- */
-
 export function twoFer(name?: String): string {
     return `One for ${name || 'you'}, one for me.`
 }
