64be8e680d1c658a2a529541383f47c21e45cece
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-05-15 00:40:42 +0530

    feat: all tests pass

--- elixir/secrets/lib/secrets.ex
+++ elixir/secrets/lib/secrets.ex
@@ -1,29 +1,29 @@
 defmodule Secrets do
   def secret_add(secret) do
-    # Please implement the secret_add/1 function
+    fn x -> x + secret end
   end
 
   def secret_subtract(secret) do
-    # Please implement the secret_subtract/1 function
+    fn x -> x - secret end
   end
 
   def secret_multiply(secret) do
-    # Please implement the secret_multiply/1 function
+    fn x -> x * secret end
   end
 
   def secret_divide(secret) do
-    # Please implement the secret_divide/1 function
+    fn x -> div(x, secret) end
   end
 
   def secret_and(secret) do
-    # Please implement the secret_and/1 function
+    fn x -> Bitwise.&&&(x, secret) end
   end
 
   def secret_xor(secret) do
-    # Please implement the secret_xor/1 function
+    fn x -> Bitwise.^^^(x, secret) end
   end
 
   def secret_combine(secret_function1, secret_function2) do
-    # Please implement the secret_combine/2 function
+    fn x -> secret_function2.(secret_function1.(x)) end
   end
 end
