bc6356123bc80819c805e1d2f7e7539fbca4d754
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-05-14 23:02:51 +0530

    feat: 10 tests pass

---
+++ elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -0,0 +1,18 @@
+defmodule FreelancerRates do
+  def daily_rate(hourly_rate) do
+    hourly_rate * 8.0
+  end
+
+  def apply_discount(before_discount, discount) do
+    before_discount - (before_discount / 100.0 * discount)
+  end
+
+  def monthly_rate(hourly_rate, discount) do
+    amount = daily_rate(hourly_rate) * 22.0
+    round(apply_discount(amount, discount))
+  end
+
+  def days_in_budget(budget, hourly_rate, discount) do
+    floor(budget / apply_discount(daily_rate(hourly_rate), discount))
+  end
+end
