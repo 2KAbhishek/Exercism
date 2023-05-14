2bd22b51a223045c1521363a2b0dc6a9c87b0160
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-05-14 12:25:05 +0530

    feat: lasagna tests pass

--- elixir/lasagna/lib/lasagna.ex
+++ elixir/lasagna/lib/lasagna.ex
@@ -1,11 +1,13 @@
 defmodule Lasagna do
-  # Please define the 'expected_minutes_in_oven/0' function
+  def expected_minutes_in_oven, do: 40
 
-  # Please define the 'remaining_minutes_in_oven/1' function
+  def remaining_minutes_in_oven(elapsed_minutes), do: expected_minutes_in_oven - elapsed_minutes
 
-  # Please define the 'preparation_time_in_minutes/1' function
+  def preparation_time_in_minutes(layers), do: layers * 2
 
-  # Please define the 'total_time_in_minutes/2' function
+  def total_time_in_minutes(layers, elapsed_minutes) do
+    preparation_time_in_minutes(layers) + elapsed_minutes
+  end
 
-  # Please define the 'alarm/0' function
+  def alarm, do: "Ding!"
 end
