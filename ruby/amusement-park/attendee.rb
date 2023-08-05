57e850575e87593765097d8c89b9ae8cecd185af
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-08-06 00:29:29 +0530

    feat: implement amusement park

--- ruby/amusement-park/attendee.rb
+++ ruby/amusement-park/attendee.rb
@@ -1,21 +1,21 @@
 class Attendee
   def initialize(height)
-    raise 'Implement the Attendee#initialize method'
+    @height = height
   end
 
   def height
-    raise 'Implement the Attendee#height method'
+    @height
   end
 
   def pass_id
-    raise 'Implement the Attendee#pass_id method'
+    @pass_id
   end
 
   def issue_pass!(pass_id)
-    raise 'Implement the Attendee#issue_pass! method'
+    @pass_id = pass_id
   end
 
   def revoke_pass!
-    raise 'Implement the Attendee#revoke_pass! method'
+    @pass_id = nil
   end
 end
