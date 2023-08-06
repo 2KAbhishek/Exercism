e51af45efd96908c49f8bef41e71bb58d6db8fd7
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-08-06 22:22:11 +0530

    feat: implement bird count

--- ruby/bird-count/bird_count.rb
+++ ruby/bird-count/bird_count.rb
@@ -1,25 +1,25 @@
 class BirdCount
   def self.last_week
-    raise 'Please implement the BirdCount.last_week method'
+    [0, 2, 5, 3, 7, 8, 4]
   end
 
   def initialize(birds_per_day)
-    raise 'Please implement the BirdCount#initialize method'
+    @birds_per_day = birds_per_day
   end
 
   def yesterday
-    raise 'Please implement the BirdCount#yesterday method'
+    @birds_per_day[-2]
   end
 
   def total
-    raise 'Please implement the BirdCount#total method'
+    @birds_per_day.sum
   end
 
   def busy_days
-    raise 'Please implement the BirdCount#busy_days method'
+    @birds_per_day.count { |birds| birds >= 5 }
   end
 
   def day_without_birds?
-    raise 'Please implement the BirdCount#day_without_birds method'
+    @birds_per_day.any?(&:zero?)
   end
 end
