cd76ffdb79974a02c865d763959fd69e7f99fb9b
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-08-06 21:38:21 +0530

    feat: all tests pass

--- ruby/savings-account/savings_account.rb
+++ ruby/savings-account/savings_account.rb
@@ -1,13 +1,26 @@
 module SavingsAccount
   def self.interest_rate(balance)
-    raise 'Please implement the SavingsAccount.interest_rate method'
+    if balance.negative?
+      3.213
+    elsif balance < 1000
+      0.5
+    elsif balance < 5000
+      1.621
+    else
+      2.475
+    end
   end
 
   def self.annual_balance_update(balance)
-    raise 'Please implement the SavingsAccount.annual_balance_update method'
+    balance + (interest_rate(balance) * (balance / 100))
   end
 
   def self.years_before_desired_balance(current_balance, desired_balance)
-    raise 'Please implement the SavingsAccount.years_before_desired_balance method'
+    years = 0
+    until current_balance >= desired_balance
+      years += 1
+      current_balance = annual_balance_update(current_balance)
+    end
+    years
   end
 end
