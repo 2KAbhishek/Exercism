fbde500e79cbf3caebf63c508ebe7a980c8c3a0b
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-08-06 22:03:11 +0530

    init: bird-count ruby

---
+++ ruby/bird-count/bird_count.rb
@@ -0,0 +1,25 @@
+class BirdCount
+  def self.last_week
+    raise 'Please implement the BirdCount.last_week method'
+  end
+
+  def initialize(birds_per_day)
+    raise 'Please implement the BirdCount#initialize method'
+  end
+
+  def yesterday
+    raise 'Please implement the BirdCount#yesterday method'
+  end
+
+  def total
+    raise 'Please implement the BirdCount#total method'
+  end
+
+  def busy_days
+    raise 'Please implement the BirdCount#busy_days method'
+  end
+
+  def day_without_birds?
+    raise 'Please implement the BirdCount#day_without_birds method'
+  end
+end
