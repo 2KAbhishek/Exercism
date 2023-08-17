b191fb8d0d0e3574f2206b414e8a10495112d11c
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-08-17 16:56:54 +0530

    refactor: inline methods

--- elixir/language-list/lib/language_list.ex
+++ elixir/language-list/lib/language_list.ex
@@ -1,25 +1,13 @@
 defmodule LanguageList do
-  def new() do
-    []
-  end
+  def new(), do: []
 
-  def add(list, language) do
-    [language | list]
-  end
+  def add(list, language), do: [language | list]
 
-  def remove(list) do
-    tl(list)
-  end
+  def remove(list), do: tl(list)
 
-  def first(list) do
-    hd(list)
-  end
+  def first(list), do: hd(list)
 
-  def count(list) do
-    length(list)
-  end
+  def count(list), do: length(list)
 
-  def functional_list?(list) do
-    "Elixir" in list
-  end
+  def functional_list?(list), do: "Elixir" in list
 end
