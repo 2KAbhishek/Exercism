1c8cad9bf07c2bad0d10d7f81870da72988b7321
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-08-17 13:42:49 +0530

    feat: implement soln

--- ruby/simple-calculator/simple_calculator.rb
+++ ruby/simple-calculator/simple_calculator.rb
@@ -1,7 +1,19 @@
+# frozen_string_literal: true
+
 class SimpleCalculator
+  class UnsupportedOperation < StandardError; end
+
   ALLOWED_OPERATIONS = ['+', '/', '*'].freeze
 
   def self.calculate(first_operand, second_operand, operation)
-    raise 'Please implement the SimpleCalculator.calculate method'
+    raise ArgumentError unless first_operand.is_a?(Integer) && second_operand.is_a?(Integer)
+    raise UnsupportedOperation unless ALLOWED_OPERATIONS.include?(operation)
+
+    begin
+      res = first_operand.send(operation, second_operand)
+      "#{first_operand} #{operation} #{second_operand} = #{res}"
+    rescue ZeroDivisionError
+      'Division by zero is not allowed.'
+    end
   end
 end
