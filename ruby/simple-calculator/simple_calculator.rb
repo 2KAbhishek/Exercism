ff138ae933b5e7e1105ab7c8121496180c244b39
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-08-17 13:18:49 +0530

    init: simple calc ruby

---
+++ ruby/simple-calculator/simple_calculator.rb
@@ -0,0 +1,7 @@
+class SimpleCalculator
+  ALLOWED_OPERATIONS = ['+', '/', '*'].freeze
+
+  def self.calculate(first_operand, second_operand, operation)
+    raise 'Please implement the SimpleCalculator.calculate method'
+  end
+end
