ceba3e49fd145afce83cf168d002a11db62eb42e
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-08-17 11:37:00 +0530

    refactor: methods

--- ruby/boutique-inventory/boutique_inventory.rb
+++ ruby/boutique-inventory/boutique_inventory.rb
@@ -6,23 +6,23 @@ class BoutiqueInventory
   end
 
   def item_names
-    @items.map { |item| item[:name] }.flatten.sort
+    items.map { |item| item[:name] }.sort
   end
 
   def cheap
-    @items.select { |item| item[:price] < 30 }
+    items.select { |item| item[:price] < 30 }
   end
 
   def out_of_stock
-    @items.select { |item| item[:quantity_by_size].values.sum.zero? }
+    items.select { |item| item[:quantity_by_size].empty? }
   end
 
   def stock_for_item(name)
-    @items.find { |item| item[:name] == name }[:quantity_by_size]
+    items.find { |item| item[:name] == name }[:quantity_by_size]
   end
 
   def total_stock
-    @items.map { |item| item[:quantity_by_size].values.sum }.sum
+    items.map { |item| item[:quantity_by_size].values.sum }.sum
   end
 
   private
