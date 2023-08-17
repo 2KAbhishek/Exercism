0cb6caf05cfe17d4281eba362855310091919a00
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-08-17 11:33:34 +0530

    feat: implement soln

--- ruby/boutique-inventory/boutique_inventory.rb
+++ ruby/boutique-inventory/boutique_inventory.rb
@@ -1,28 +1,31 @@
+# frozen_string_literal: true
+
 class BoutiqueInventory
   def initialize(items)
     @items = items
   end
 
   def item_names
-    raise 'Implement the BoutiqueInventory#item_names method'
+    @items.map { |item| item[:name] }.flatten.sort
   end
 
   def cheap
-    raise 'Implement the BoutiqueInventory#cheap method'
+    @items.select { |item| item[:price] < 30 }
   end
 
   def out_of_stock
-    raise 'Implement the BoutiqueInventory#out_of_stock method'
+    @items.select { |item| item[:quantity_by_size].values.sum.zero? }
   end
 
   def stock_for_item(name)
-    raise 'Implement the BoutiqueInventory#stock_for_item method'
+    @items.find { |item| item[:name] == name }[:quantity_by_size]
   end
 
   def total_stock
-    raise 'Implement the BoutiqueInventory#total_stock method'
+    @items.map { |item| item[:quantity_by_size].values.sum }.sum
   end
 
   private
+
   attr_reader :items
 end
