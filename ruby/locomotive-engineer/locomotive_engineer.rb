7cd6ad009f86820f0f81b36af17787558a6e8a72
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-08-17 13:15:13 +0530

    feat: implement soln

--- ruby/locomotive-engineer/locomotive_engineer.rb
+++ ruby/locomotive-engineer/locomotive_engineer.rb
@@ -1,17 +1,18 @@
+# frozen_string_literal: true
+
 class LocomotiveEngineer
-  def self.generate_list_of_wagons
-    raise 'Please implement the LocomotiveEngineer.generate_list_of_wagons method'
-  end
+  def self.generate_list_of_wagons(*wagons) = wagons
 
   def self.fix_list_of_wagons(each_wagons_id, missing_wagons)
-    raise 'Please implement the LocomotiveEngineer.fix_list_of_wagons method'
+    first, second, third, *rest = each_wagons_id
+    [third, *missing_wagons, *rest, first, second]
   end
 
-  def self.add_missing_stops
-    raise 'Please implement the LocomotiveEngineer.add_missing_stops method'
+  def self.add_missing_stops(routing_hash, **missing_stops)
+    { **routing_hash, stops: missing_stops.sort.to_h.values }
   end
 
   def self.extend_route_information(route, more_route_information)
-    raise 'Please implement the LocomotiveEngineer.extend_route_information method'
+    { **route, **more_route_information }
   end
 end
