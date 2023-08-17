570317fca84210f4cca81d7a20d0ea50842d14d1
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-08-17 13:55:09 +0530

    feat: implement moviegoer

--- ruby/moviegoer/moviegoer.rb
+++ ruby/moviegoer/moviegoer.rb
@@ -1,23 +1,27 @@
+# frozen_string_literal: true
+
 # This is a custom exception that you can use in your code
 class NotMovieClubMemberError < RuntimeError
 end
 
 class Moviegoer
+  attr_reader :age, :member
+
   def initialize(age, member: false)
     @age = age
     @member = member
   end
 
   def ticket_price
-    raise 'Please implement the Moviegoer#ticket_price method'
+    age >= 60 ? 10 : 15
   end
 
   def watch_scary_movie?
-    raise 'Please implement the Moviegoer#watch_scary_movie method'
+    age >= 18
   end
 
   # Popcorn is 🍿
   def claim_free_popcorn!
-    raise 'Please implement the Moviegoer#claim_free_popcorn method'
+    member == true ? '🍿' : (raise NotMovieClubMemberError)
   end
 end
