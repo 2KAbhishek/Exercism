c29be7542295b843a5719d5ef2b53145f5e38747
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-08-19 10:43:21 +0530

    feat(wip): log level implementation

--- elixir/log-level/lib/log_level.ex
+++ elixir/log-level/lib/log_level.ex
@@ -1,6 +1,14 @@
 defmodule LogLevel do
   def to_label(level, legacy?) do
-    # Please implement the to_label/2 function
+    cond do
+      level == 0 && !legacy? -> :trace
+      level == 1 -> :debug
+      level == 2 -> :info
+      level == 3 -> :warning
+      level == 4 -> :error
+      level == 5 && !legacy? -> :fatal
+      -> :unknown
+    end
   end
 
   def alert_recipient(level, legacy?) do
