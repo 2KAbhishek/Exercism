2cb89e6d692ac2d6f74ae4eb96aa3123c1282480
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-08-19 12:27:37 +0530

    refactor: smarter clause

--- elixir/guessing-game/lib/guessing_game.ex
+++ elixir/guessing-game/lib/guessing_game.ex
@@ -1,8 +1,7 @@
 defmodule GuessingGame do
-  def compare(secret_number, guess), do: "Correct"
+  def compare(secret_number, secret_number), do: "Correct"
   def compare(_, guess \\ :no_guess) when not is_integer(guess), do: "Make a guess"
   def compare(secret_number, guess) when abs(secret_number - guess) == 1, do: "So close"
   def compare(secret_number, guess) when secret_number < guess, do: "Too high"
-  def compare(secret_number, guess) when secret_number > guess, do: "Too low"
+  def compare(secret_number, guess) when guess < secret_number, do: "Too low"
 end
-
