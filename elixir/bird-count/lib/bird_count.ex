345e41cfa95c57d11d075056d1c3c88a9d7aa109
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-08-26 22:23:41 +0530

    feat: implement recursive soln

--- elixir/bird-count/lib/bird_count.ex
+++ elixir/bird-count/lib/bird_count.ex
@@ -1,21 +1,20 @@
 defmodule BirdCount do
-  def today(list) do
-    # Please implement the today/1 function
-  end
+  def today([]), do: nil
+  def today([head | _]), do: head
 
-  def increment_day_count(list) do
-    # Please implement the increment_day_count/1 function
-  end
+  def increment_day_count([]), do: [1]
+  def increment_day_count([head | tail]), do: [head + 1 | tail]
 
-  def has_day_without_birds?(list) do
-    # Please implement the has_day_without_birds?/1 function
-  end
+  def has_day_without_birds?([]), do: false
+  def has_day_without_birds?([0 | _]), do: true
+  def has_day_without_birds?([_ | tail]), do: has_day_without_birds?(tail)
 
-  def total(list) do
-    # Please implement the total/1 function
-  end
+  def total(list), do: total(list, 0)
+  def total([], count), do: count
+  def total([head | tail], count), do: total(tail, head + count)
 
-  def busy_days(list) do
-    # Please implement the busy_days/1 function
-  end
+  def busy_days(list), do: busy_days(list, 0)
+  def busy_days([], count), do: count
+  def busy_days([head | tail], count) when head >= 5, do: busy_days(tail, count + 1)
+  def busy_days([_ | tail], count), do: busy_days(tail, count)
 end
