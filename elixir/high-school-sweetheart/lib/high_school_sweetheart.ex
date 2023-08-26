9ce089180efd248bff346613bd739086f30373fe
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-08-26 13:53:13 +0530

    feat: implement pipe based soln

--- elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
+++ elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -1,32 +1,36 @@
 defmodule HighSchoolSweetheart do
-  def first_letter(name) do
-    # Please implement the first_letter/1 function
-  end
+  def first_letter(name), do: name |> String.trim |> String.first
 
   def initial(name) do
-    # Please implement the initial/1 function
+    name
+    |> first_letter
+    |> Kernel.<>(".")
+    |> String.upcase
   end
 
   def initials(full_name) do
-    # Please implement the initials/1 function
+    full_name
+    |> String.split(" ")
+    |> Enum.map(&initial/1)
+    |> Enum.join(" ")
   end
 
   def pair(full_name1, full_name2) do
-    #      ******       ******
-    #    **      **   **      **
-    #  **         ** **         **
-    # **            *            **
-    # **                         **
-    # **     X. X.  +  X. X.     **
-    #  **                       **
-    #    **                   **
-    #      **               **
-    #        **           **
-    #          **       **
-    #            **   **
-    #              ***
-    #               *
-
-    # Please implement the pair/2 function
+    """
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    **     #{initials(full_name1)}  +  #{initials(full_name2)}     **
+     **                       **
+       **                   **
+         **               **
+           **           **
+             **       **
+               **   **
+                 ***
+                  *
+    """
   end
 end
