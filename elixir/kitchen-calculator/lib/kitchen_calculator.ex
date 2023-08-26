1bf7767a6892393260126e3821b7234af9673498
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-08-26 12:17:16 +0530

    feat: implement soln

--- elixir/kitchen-calculator/lib/kitchen_calculator.ex
+++ elixir/kitchen-calculator/lib/kitchen_calculator.ex
@@ -1,17 +1,21 @@
 defmodule KitchenCalculator do
   def get_volume(volume_pair) do
-    # Please implement the get_volume/1 function
+    elem(volume_pair, 1)
   end
 
-  def to_milliliter(volume_pair) do
-    # Please implement the to_milliliter/1 functions
-  end
+  def to_milliliter({:milliliter, value}), do: {:milliliter, value}
+  def to_milliliter({:cup, value}), do: {:milliliter, value * 240}
+  def to_milliliter({:fluid_ounce, value}), do: {:milliliter, value * 30}
+  def to_milliliter({:teaspoon, value}), do: {:milliliter, value * 5}
+  def to_milliliter({:tablespoon, value}), do: {:milliliter, value * 15}
 
-  def from_milliliter(volume_pair, unit) do
-    # Please implement the from_milliliter/2 functions
-  end
+  def from_milliliter({:milliliter, value}, :milliliter), do: {:milliliter, value}
+  def from_milliliter({:milliliter, value}, :cup), do: {:cup, value / 240}
+  def from_milliliter({:milliliter, value}, :fluid_ounce), do: {:fluid_ounce, value / 30}
+  def from_milliliter({:milliliter, value}, :teaspoon), do: {:teaspoon, value / 5}
+  def from_milliliter({:milliliter, value}, :tablespoon), do: {:tablespoon, value / 15}
 
   def convert(volume_pair, unit) do
-    # Please implement the convert/2 function
+    from_milliliter(to_milliliter(volume_pair), unit)
   end
 end
