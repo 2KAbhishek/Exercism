eff0a0f0a0f184057913593e89ac6124f99ffd5a
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-08-27 17:04:09 +0530

    refactor: implement with pipe

--- elixir/name-badge/lib/name_badge.ex
+++ elixir/name-badge/lib/name_badge.ex
@@ -1,7 +1,11 @@
 defmodule NameBadge do
+  @owner_dep "OWNER"
+  @separator " - "
+
   def print(id, name, department) do
-    id_str = if id, do: "[#{id}] - ", else: ""
-    dpt_str = if department, do: " - " <> String.upcase(department), else: " - OWNER"
-    id_str <> name <> dpt_str
+    id_part = if id, do: "[#{id}]"
+    dep_part = if department, do: String.upcase(department), else: @owner_dep
+
+    [id_part, name, dep_part] |> Enum.reject(&is_nil/1) |> Enum.join(@separator)
   end
 end
