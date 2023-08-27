8ac147e776c2abdd94962c4639a66c765340be7a
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-08-27 16:00:25 +0530

    feat: soln with pipes and IO

--- elixir/rpg-character-sheet/lib/rpg/character_sheet.ex
+++ elixir/rpg-character-sheet/lib/rpg/character_sheet.ex
@@ -1,21 +1,29 @@
 defmodule RPG.CharacterSheet do
-  def welcome() do
-    # Please implement the welcome/0 function
-  end
+  def welcome(), do: IO.puts("Welcome! Let's fill out your character sheet together.")
 
   def ask_name() do
-    # Please implement the ask_name/0 function
+    name = IO.gets("What is your character's name?\n")
+    String.trim(name)
   end
 
   def ask_class() do
-    # Please implement the ask_class/0 function
+    class = IO.gets("What is your character's class?\n")
+    String.trim(class)
   end
 
   def ask_level() do
-    # Please implement the ask_level/0 function
+    level = IO.gets("What is your character's level?\n")
+    level |> String.trim |> String.to_integer
   end
 
   def run() do
-    # Please implement the run/0 function
+    welcome()
+    character = %{}
+      |> Map.put(:name, ask_name())
+      |> Map.put(:class, ask_class())
+      |> Map.put(:level, ask_level())
+
+    IO.puts("Your character: #{inspect(character)}")
+    character
   end
 end
