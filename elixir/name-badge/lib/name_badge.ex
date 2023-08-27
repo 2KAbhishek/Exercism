85408edf601fec75fd5e7938a341b8565d39d515
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-08-27 17:01:59 +0530

    feat: implement sol withh if

--- elixir/name-badge/lib/name_badge.ex
+++ elixir/name-badge/lib/name_badge.ex
@@ -1,5 +1,7 @@
 defmodule NameBadge do
   def print(id, name, department) do
-    # Please implement the print/3 function
+    id_str = if id, do: "[#{id}] - ", else: ""
+    dpt_str = if department, do: " - " <> String.upcase(department), else: " - OWNER"
+    id_str <> name <> dpt_str
   end
 end
