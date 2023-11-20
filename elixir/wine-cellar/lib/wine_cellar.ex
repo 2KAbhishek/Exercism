dcfe97006ac1c5dc536ef343427ee7eea6e86629
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-11-20 15:23:20 +0530

    feat: implement wine cellar

--- elixir/wine-cellar/lib/wine_cellar.ex
+++ elixir/wine-cellar/lib/wine_cellar.ex
@@ -1,11 +1,20 @@
 defmodule WineCellar do
   def explain_colors do
-    # Please implement the explain_colors/0 function
+    [
+      white: "Fermented without skin contact.",
+      red: "Fermented with skin contact using dark-colored grapes.",
+      rose: "Fermented with some skin contact, but not enough to qualify as a red wine."
+    ]
   end
 
   def filter(cellar, color, opts \\ []) do
-    # Please implement the filter/3 function
+    Keyword.get_values(cellar, color)
+    |> filter_by_year(opts[:year])
+    |> filter_by_country(opts[:country])
   end
+
+  defp filter_by_year(wines, nil), do: wines
+  defp filter_by_country(wines, nil), do: wines
 
   # The functions below do not need to be modified.
 
