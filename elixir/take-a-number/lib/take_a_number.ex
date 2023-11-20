77ae7a1bf96c9ea1a2ec2321939014b13496d4fa
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-11-20 14:36:18 +0530

    feat: implement take a number

--- elixir/take-a-number/lib/take_a_number.ex
+++ elixir/take-a-number/lib/take_a_number.ex
@@ -1,5 +1,24 @@
 defmodule TakeANumber do
   def start() do
-    # Please implement the start/0 function
+    spawn(fn -> loop(0) end)
+  end
+
+  defp loop(state) do
+    receive do
+      {:report_state, sender_pid} ->
+        send(sender_pid, state)
+        loop(state)
+
+      {:take_a_number, sender_pid} ->
+        state = state + 1
+        send(sender_pid, state)
+        loop(state)
+
+      :stop ->
+        :ok
+
+      _ ->
+        loop(state)
+    end
   end
 end
