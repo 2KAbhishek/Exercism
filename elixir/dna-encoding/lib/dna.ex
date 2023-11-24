8ec512ebd16ef60a96b7de3f89761fd5508bd28a
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-11-25 01:18:16 +0530

    fix: space in map

--- elixir/dna-encoding/lib/dna.ex
+++ elixir/dna-encoding/lib/dna.ex
@@ -1,18 +1,18 @@
 defmodule DNA do
-  @map %{
+  @dna_map %{
     ?A => 0b0001,
     ?C => 0b0010,
     ?G => 0b0100,
     ?T => 0b1000,
-    " " => 0b0000
+    ?\s => 0b0000
   }
 
   def encode_nucleotide(code_point) do
-    @map[code_point]
+    @dna_map[code_point]
   end
 
   def decode_nucleotide(encoded_code) do
-    Enum.find(@map, fn {_, value} -> value == encoded_code end) |> elem(0)
+    Enum.find(@dna_map, fn {_, value} -> value == encoded_code end) |> elem(0)
   end
 
   def encode(dna), do: do_encode(dna, <<>>)
