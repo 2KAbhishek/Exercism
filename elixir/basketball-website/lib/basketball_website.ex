a0f6fb9f11fcf3cf35a6d7b145450d8c21f42c37
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-11-25 12:43:56 +0530

    feat: impl extract from path

--- elixir/basketball-website/lib/basketball_website.ex
+++ elixir/basketball-website/lib/basketball_website.ex
@@ -1,6 +1,12 @@
 defmodule BasketballWebsite do
   def extract_from_path(data, path) do
-    # Please implement the extract_from_path/2 function
+    do_extract_from_path(data, String.split(path, "."))
+  end
+
+  defp do_extract_from_path(data, []), do: data
+
+  defp do_extract_from_path(data, [head | tail]) do
+    do_extract_from_path(data[head], tail)
   end
 
   def get_in_path(data, path) do
