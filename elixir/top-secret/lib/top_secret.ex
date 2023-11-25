394f9ee3dfde145a4097ee6c97a3c632aa61a06f
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-11-25 19:27:13 +0530

    feat: impl to ast

--- elixir/top-secret/lib/top_secret.ex
+++ elixir/top-secret/lib/top_secret.ex
@@ -1,6 +1,6 @@
 defmodule TopSecret do
   def to_ast(string) do
-    # Please implement the to_ast/1 function
+    Code.string_to_quoted!(string)
   end
 
   def decode_secret_message_part(ast, acc) do
