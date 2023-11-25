c6428c460303f0283d4364d93f327286a7f6b243
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-11-25 17:23:03 +0530

    feat: impl type from extension

--- elixir/file-sniffer/lib/file_sniffer.ex
+++ elixir/file-sniffer/lib/file_sniffer.ex
@@ -1,6 +1,14 @@
 defmodule FileSniffer do
   def type_from_extension(extension) do
-    # Please implement the type_from_extension/1 function
+    extension_map = %{
+      exe: "application/octet-stream",
+      bmp: "image/bmp",
+      png: "image/png",
+      jpg: "image/jpg",
+      gif: "image/gif"
+    }
+
+    extension_map[String.to_atom(extension)]
   end
 
   def type_from_binary(file_binary) do
