92c35795d22081af30c5b61860007fc63e58d22e
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-11-25 17:44:08 +0530

    feat: impl type from file binary

--- elixir/file-sniffer/lib/file_sniffer.ex
+++ elixir/file-sniffer/lib/file_sniffer.ex
@@ -1,18 +1,24 @@
 defmodule FileSniffer do
   def type_from_extension(extension) do
-    extension_map = %{
-      exe: "application/octet-stream",
-      bmp: "image/bmp",
-      png: "image/png",
-      jpg: "image/jpg",
-      gif: "image/gif"
-    }
-
-    extension_map[String.to_atom(extension)]
+    case extension do
+      "exe" -> "application/octet-stream"
+      "bmp" -> "image/bmp"
+      "png" -> "image/png"
+      "jpg" -> "image/jpg"
+      "gif" -> "image/gif"
+      _ -> nil
+    end
   end
 
   def type_from_binary(file_binary) do
-    # Please implement the type_from_binary/1 function
+    case file_binary do
+      <<0x7F, 0x45, 0x4C, 0x46, _::binary>> -> "application/octet-stream"
+      <<0x42, 0x4D, _::binary>> -> "image/bmp"
+      <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, _::binary>> -> "image/png"
+      <<0xFF, 0xD8, 0xFF, _::binary>> -> "image/jpg"
+      <<0x47, 0x49, 0x46, 0x38, _::binary>> -> "image/gif"
+      _ -> nil
+    end
   end
 
   def verify(file_binary, extension) do
