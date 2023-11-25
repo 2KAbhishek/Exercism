9958ec217c1a506d98599e27fdb40fc1128b4ccd
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-11-25 16:56:12 +0530

    refactor: use & shorthand

--- elixir/boutique-inventory/lib/boutique_inventory.ex
+++ elixir/boutique-inventory/lib/boutique_inventory.ex
@@ -1,26 +1,22 @@
 defmodule BoutiqueInventory do
   def sort_by_price(inventory) do
-    Enum.sort_by(inventory, fn item -> item.price end)
+    Enum.sort_by(inventory, & &1.price)
   end
 
   def with_missing_price(inventory) do
-    Enum.filter(inventory, fn item -> item.price == nil end)
+    Enum.filter(inventory, &(&1.price == nil))
   end
 
   def update_names(inventory, old_word, new_word) do
     Enum.map(
       inventory,
-      fn item -> %{item | name: String.replace(item.name, old_word, new_word)} end
+      &%{&1 | name: String.replace(&1.name, old_word, new_word)}
     )
   end
 
   def increase_quantity(item, count) do
-    Map.put(
-      item,
-      :quantity_by_size,
-      Enum.map(item.quantity_by_size, fn {size, quantity} -> {size, quantity + count} end)
-      |> Enum.into(%{})
-    )
+    new_quantity = Map.new(item.quantity_by_size, fn {k, v} -> {k, v + count} end)
+    %{item | quantity_by_size: new_quantity}
   end
 
   def total_quantity(item) do
