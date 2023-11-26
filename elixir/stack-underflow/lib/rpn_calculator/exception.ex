75c77bb10fa010fec9ed8803e1d883c2b6294253
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-11-26 18:54:33 +0530

    feat: impl div by zero ex

--- elixir/stack-underflow/lib/rpn_calculator/exception.ex
+++ elixir/stack-underflow/lib/rpn_calculator/exception.ex
@@ -1,5 +1,8 @@
 defmodule RPNCalculator.Exception do
   # Please implement DivisionByZeroError here.
+  defmodule DivisionByZeroError do
+    defexception message: "division by zero occurred"
+  end
 
   # Please implement StackUnderflowError here.
 end
