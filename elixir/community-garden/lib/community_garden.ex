c2c0f40fea6b05a6bc9ea06efd48dfafbdd0c7a6
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-11-26 10:51:30 +0530

    feat: impl register

--- elixir/community-garden/lib/community_garden.ex
+++ elixir/community-garden/lib/community_garden.ex
@@ -6,15 +6,21 @@ end
 
 defmodule CommunityGarden do
   def start(opts \\ []) do
-    Agent.start_link(fn -> [] end, opts)
+    Agent.start(fn -> {1, []} end, opts)
   end
 
   def list_registrations(pid) do
-    Agent.get(pid, fn registrations -> registrations end)
+    Agent.get(pid, fn {_next_plot, state} -> state end)
   end
 
   def register(pid, register_to) do
-    # Please implement the register/2 function
+    Agent.get_and_update(
+      pid,
+      fn {next_plot_id, state} ->
+        new_plot = %Plot{plot_id: next_plot_id, registered_to: register_to}
+        {new_plot, {next_plot_id + 1, [new_plot | state]}}
+      end
+    )
   end
 
   def release(pid, plot_id) do
