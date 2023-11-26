10efe82b8ac9b6bfb96d26fb3673c5ef2f22b98f
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-11-26 07:57:38 +0530

    feat: impl cartesian combinations

--- elixir/boutique-suggestions/lib/boutique_suggestions.ex
+++ elixir/boutique-suggestions/lib/boutique_suggestions.ex
@@ -1,5 +1,7 @@
 defmodule BoutiqueSuggestions do
-  def get_combinations(tops, bottoms, options) do
-    # Please implement the get_combinations/3 function
+  def get_combinations(tops, bottoms, options \\ []) do
+    for top <- tops, bottom <- bottoms do
+      {top, bottom}
+    end
   end
 end
