cc2511293a1d5151e0cf16f4c4df95600a72b941
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-11-26 07:25:57 +0530

    feat: impl new/1

--- elixir/remote-control-car/lib/remote_control_car.ex
+++ elixir/remote-control-car/lib/remote_control_car.ex
@@ -2,13 +2,9 @@ defmodule RemoteControlCar do
   @enforce_keys [:nickname]
   defstruct [:nickname, battery_percentage: 100, distance_driven_in_meters: 0]
 
-  def new() do
-    %RemoteControlCar{nickname: "none"}
-  end
+  def new(), do: %RemoteControlCar{nickname: "none"}
 
-  def new(nickname) do
-    # Please implement the new/1 function
-  end
+  def new(nickname), do: %RemoteControlCar{nickname: nickname}
 
   def display_distance(remote_car) do
     # Please implement the display_distance/1 function
