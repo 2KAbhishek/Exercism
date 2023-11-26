d92055b9d316164dc4ac73f81ed29c9af81fa871
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-11-26 08:05:50 +0530

    feat: impl max price filter

--- elixir/boutique-suggestions/lib/boutique_suggestions.ex
+++ elixir/boutique-suggestions/lib/boutique_suggestions.ex
@@ -1,6 +1,11 @@
 defmodule BoutiqueSuggestions do
   def get_combinations(tops, bottoms, options \\ []) do
-    for top <- tops, bottom <- bottoms, top[:base_color] != bottom[:base_color] do
+    max_price = Keyword.get(options, :maximum_price, 100)
+
+    for top <- tops,
+        bottom <- bottoms,
+        top.base_color != bottom.base_color,
+        top.price + bottom.price <= max_price do
       {top, bottom}
     end
   end
