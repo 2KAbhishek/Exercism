fff79474901d5a14b2971da33f5d5024195ec8ea
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-11-26 19:03:02 +0530

    feat: impl stack underflow with context

--- elixir/stack-underflow/lib/rpn_calculator/exception.ex
+++ elixir/stack-underflow/lib/rpn_calculator/exception.ex
@@ -7,5 +7,12 @@ defmodule RPNCalculator.Exception do
   # Please implement StackUnderflowError here.
   defmodule StackUnderflowError do
     defexception message: "stack underflow occurred"
+
+    def exception(context) do
+      case context do
+        [] -> %__MODULE__{}
+        context -> %__MODULE__{message: "stack underflow occurred, context: #{context}"}
+      end
+    end
   end
 end
