f7831199d7ea7c8654bbe4b19171c9a7748f09fe
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-11-26 07:44:56 +0530

    refactor: default arg in new

--- elixir/remote-control-car/lib/remote_control_car.ex
+++ elixir/remote-control-car/lib/remote_control_car.ex
@@ -2,9 +2,7 @@ defmodule RemoteControlCar do
   @enforce_keys [:nickname]
   defstruct [:nickname, battery_percentage: 100, distance_driven_in_meters: 0]
 
-  def new(), do: %RemoteControlCar{nickname: "none"}
-
-  def new(nickname), do: %RemoteControlCar{nickname: nickname}
+  def new(nickname \\ "none"), do: %RemoteControlCar{nickname: nickname}
 
   def display_distance(car = %RemoteControlCar{}), do: "#{car.distance_driven_in_meters} meters"
 
