cd3b3faf7c17b1688d6618df351f2d48a1ddd30a
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-11-26 23:34:08 +0530

    feat: add def impl for init

--- elixir/dancing-dots/lib/dancing_dots/animation.ex
+++ elixir/dancing-dots/lib/dancing_dots/animation.ex
@@ -6,6 +6,12 @@ defmodule DancingDots.Animation do
 
   @callback init(opts :: opts) :: {:ok, opts :: opts} | {:error, error :: error}
   @callback handle_frame(dot :: dot, frame_number :: frame_number, opts :: opts) :: {dot :: dot}
+
+  defmacro __using__(_opts \\ []) do
+    quote do
+      def init(opts), do: {:ok, opts}
+    end
+  end
 end
 
 defmodule DancingDots.Flicker do
