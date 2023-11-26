e28832b2d4c9dce9f7afa0ee07e8c90dde30cc2a
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-11-26 21:14:08 +0530

    feat: impl arg check

--- elixir/lucas-numbers/lib/lucas_numbers.ex
+++ elixir/lucas-numbers/lib/lucas_numbers.ex
@@ -5,10 +5,9 @@ defmodule LucasNumbers do
 
   E.g.: 2, 1, 3, 4, 7, 11, 18, 29, ...
   """
-  def generate(1), do: [2]
-  def generate(2), do: [2, 1]
-
-  def generate(count) when count > 2 do
+  def generate(count) when is_integer(count) and count >= 1 do
     Stream.unfold({2, 1}, fn {a, b} -> {a, {b, a + b}} end) |> Enum.take(count)
   end
+
+  def generate(_), do: raise(ArgumentError, "count must be specified as an integer >= 1")
 end
