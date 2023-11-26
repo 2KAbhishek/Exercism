33090cb998aa6093cefb10e3cff9b2eef5425779
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-11-26 19:10:10 +0530

    docs: cleanup comments

--- elixir/stack-underflow/lib/rpn_calculator/exception.ex
+++ elixir/stack-underflow/lib/rpn_calculator/exception.ex
@@ -1,10 +1,8 @@
 defmodule RPNCalculator.Exception do
-  # Please implement DivisionByZeroError here.
   defmodule DivisionByZeroError do
     defexception message: "division by zero occurred"
   end
 
-  # Please implement StackUnderflowError here.
   defmodule StackUnderflowError do
     defexception message: "stack underflow occurred"
 
