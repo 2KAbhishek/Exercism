0c6851f314535abcbfbfa19073648c2d4aa31c7b
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-11-26 07:59:50 +0530

    feat: impl base color filter

--- elixir/boutique-suggestions/lib/boutique_suggestions.ex
+++ elixir/boutique-suggestions/lib/boutique_suggestions.ex
@@ -1,6 +1,6 @@
 defmodule BoutiqueSuggestions do
   def get_combinations(tops, bottoms, options \\ []) do
-    for top <- tops, bottom <- bottoms do
+    for top <- tops, bottom <- bottoms, top[:base_color] != bottom[:base_color] do
       {top, bottom}
     end
   end
